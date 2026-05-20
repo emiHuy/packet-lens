b2c4491aaf7b6b459563dc91d77d23f57f38f8f6
emiHuy <emilyhuynh06@gmail.com>
2026-05-20 02:53:24 -0400

    fix: move active tab state from Navbar.jsx to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar.jsx"
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Live"); // Options: "Live" or "Sessions"
 
   return (
     <>
-      <Navbar/>
+      <Navbar activeTab={activeTab} switchTab={setActiveTab}/>
     </>
   )
 }
