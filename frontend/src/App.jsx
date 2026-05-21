4373da654487631a60e474a96415ef9386492f88
emiHuy <emilyhuynh06@gmail.com>
2026-05-21 19:49:36 -0400

    Create idle screen for Live tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar.jsx"
+import LiveView from './views/LiveView.jsx';
 
 function App() {
   const [activeTab, setActiveTab] = useState("Live"); // Options: "Live" or "Sessions"
+  const [capturing, setCapturing] = useState(false); 
 
   return (
     <>
       <Navbar activeTab={activeTab} switchTab={setActiveTab}/>
+      <LiveView capturing={capturing} setCapturing={setCapturing}></LiveView>
     </>
   )
 }
--- frontend/src/views/LiveView.jsx
+++ frontend/src/views/LiveView.jsx
@@ -1,1 +1,24 @@
 import { Play, Square } from "lucide-react";
+import styles from "./LiveView.module.css"
+
+export default function LiveView({ capturing, setCapturing }) {
+    return (
+        <div className={styles.liveView}>
+
+            {!capturing && 
+                <div className={styles.idleWrapper}>
+                    <div className={styles.startBtnWrapper}>
+                        <div className={styles.ring}></div>
+                        <button className={styles.captureBtn} id="start-capture-btn">
+                            <Play className={styles.captureImg} size={30} color="var(--text-accented-muted)" fill="var(--text-accented-muted)" stroke="var(--text-accented)" strokeWidth={1.5}/>
+                        </button>
+                    </div>
+                    <div className={styles.startText}>
+                        <p className={styles.secondaryText}>No active capture</p>
+                        <p className={styles.tertiaryText}>press start to begin monitoring</p>
+                    </div>
+                </div>
+            }            
+        </div>
+    )
+}
