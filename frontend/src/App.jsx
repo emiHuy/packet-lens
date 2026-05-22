55f317d2d3b6fd480bbdb3080c396b4e545df699
emiHuy <emilyhuynh06@gmail.com>
2026-05-21 21:50:51 -0400

    feat: add topbar for the Live tab

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import LiveView from './views/LiveView.jsx';
 function App() {
   const [activeTab, setActiveTab] = useState("Live"); // Options: "Live" or "Sessions"
   const [capturing, setCapturing] = useState(false); 
+  const [session, setSession] = useState({});
 
   return (
     <>
       <Navbar activeTab={activeTab} switchTab={setActiveTab}/>
-      <LiveView capturing={capturing} setCapturing={setCapturing}></LiveView>
+      <LiveView capturing={capturing} setCapturing={setCapturing} session={session}></LiveView>
     </>
   )
 }
--- frontend/src/views/LiveView.jsx
+++ frontend/src/views/LiveView.jsx
@@ -1,24 +1,43 @@
 import { Play, Square } from "lucide-react";
 import styles from "./LiveView.module.css"
 
-export default function LiveView({ capturing, setCapturing }) {
+export default function LiveView({ capturing, setCapturing, session }) {
     return (
         <div className={styles.liveView}>
 
             {!capturing && 
                 <div className={styles.idleWrapper}>
+                    
                     <div className={styles.startBtnWrapper}>
                         <div className={styles.ring}></div>
                         <button className={styles.startBtn} id="start-capture-btn">
                             <Play size={30} color="var(--text-accented-muted)" fill="var(--text-accented-muted)" stroke="var(--text-accented)" strokeWidth={1.5}/>
                         </button>
                     </div>
+
                     <div className={styles.startText}>
                         <p className={styles.secondaryText}>No active capture</p>
                         <p className={styles.tertiaryText}>press start to begin monitoring</p>
                     </div>
+
                 </div>
-            }            
+            }
+
+            {capturing &&
+                <div className={styles.activeWrapper}>
+
+                    <div className={styles.topbar}>
+                        <button className={styles.stopBtn}>
+                            <Square size={15} color="var(--red)" fill="var(--red)" strokeWidth={1}></Square>
+                            Stop capture
+                        </button>
+                        <span className={styles.sessionInfo}>{session?.interface || "Unknown" } · {session.session_name || session?.session_id || "Unnamed Session"}</span>
+                    </div>
+
+                </div>
+            }
+            
+            
         </div>
     )
 }
